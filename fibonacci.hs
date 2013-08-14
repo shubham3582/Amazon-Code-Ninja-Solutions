8eb2f8f6de62d9d8334610d3e0a06e6111ff0998
Shubham Srivastava <shubham3582@gmail.com>
2013-08-14 10:03:36 +0530

    Create fibonacci.hs
    Problem Amazon Code Ninja - Fibonacci Factor
    Solution in Haskell - A bit of funny solution

---
+++ fibonacci.hs
@@ -0,0 +1,12 @@
+-- Author Shubham Srivastava. Problem Amazon Code Ninja - Fibonacci Factor
+fib = 1 : 1 : [a + b | (a, b) <- zip fib (tail fib)]
+
+ff n = head $ filter ((> 1) . (gcd n)) fib
+
+tst = do
+  n <- getLine
+  let n' = read n
+  let x = ff n'
+  putStrLn $ show x ++ " " ++ (show $ gcd x n')
+
+main = getLine >>= \t -> mapM_ (const tst) [1 .. (read t)]
